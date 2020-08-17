418700641a288c6ebe92dc76cdff5ddeb8c7968d
yu <2497075649@qq.com>
2020-08-17 17:14:05 +0800

    更新学习笔记

--- PythonSeniorCode.py
+++ PythonSeniorCode.py
@@ -460,13 +460,13 @@ def sayhello(*args,**kwargs):
 #
 # A(0)
 
-def method(self):
-    return 1
-
-klass = type('MyClass',(object,),{'method':method})
-
-instance = klass()
-print(instance.method())
+# def method(self):
+#     return 1
+#
+# klass = type('MyClass',(object,),{'method':method})
+#
+# instance = klass()
+# print(instance.method())
 
 # class MyClass():
 #     def method(self):
@@ -535,8 +535,200 @@ print(instance.method())
 #
 # a(cc='ss')
 
-def a(b,c):
-    assert isinstance(b,(int,float)),'类型错误'
-    print(b,c)
+# def a(b,c):
+#     assert isinstance(b,(int,float)),'类型错误'
+#     print(b,c)
+#
+# a('2',2)
 
-a('2',2)
+# from threading import Thread
+# import time
+#
+# def say_hello():
+#
+#     for i in range(10):
+#         time.sleep(0.2)
+#         print("hello world")
+#
+# def say_dance():
+#
+#     for j in range(10):
+#         time.sleep(0.2)
+#         print("dancing")
+
+
+
+# t_list = [Thread(target=say_hello),Thread(target=say_dance)]
+# [t.start() for t in t_list]
+
+# [t.start() for t in [t1,t2]]
+# for t in t_list:
+#     t.start()
+
+# from gmaps import Geocoding
+#
+# api = Geocoding()
+#
+# geocoded = api.geocode('Warsaw')[0]
+# print(geocoded)
+
+
+# from urllib.request import urlopen
+# from bs4 import BeautifulSoup
+#
+#
+# def query(lat, loc):
+#     url = "http://maps.google.cn/maps/api/geocode/xml?latlng=%.6f,%.6f&sensor=false" % (lat, loc)
+#     html = urlopen(url)
+#     bsObj = BeautifulSoup(html, "html.parser")
+#     addr = bsObj.find('formatted_address')
+#     return addr
+#
+#
+# print(query(26.567175,106.71545))
+
+
+# import threading
+# import time
+#
+# exitFlag = 0
+#
+# class myThread (threading.Thread):
+#     def __init__(self, threadID, name, counter):
+#         threading.Thread.__init__(self)
+#         self.threadID = threadID
+#         self.name = name
+#         self.counter = counter
+#     def run(self):
+#         print ("开始线程：" + self.name)
+#         print_time(self.name, self.counter, 5)
+#         print ("退出线程：" + self.name)
+#
+# def print_time(threadName, delay, counter):
+#     while counter:
+#         if exitFlag:
+#             threadName.exit()
+#         time.sleep(delay)
+#         print ("%s: %s" % (threadName, time.ctime(time.time())))
+#         counter -= 1
+#
+# # 创建新线程
+# thread1 = myThread(1, "Thread-1", 1)
+# thread2 = myThread(2, "Thread-2", 2)
+#
+# # 开启新线程
+# thread1.start()
+# thread2.start()
+# # 等待至线程终止
+# thread1.join()
+# thread2.join()
+# print ("退出主线程")
+
+"""
+开始线程：Thread-1
+开始线程：Thread-2
+Thread-1: Mon Aug 17 13:54:44 2020
+Thread-1: Mon Aug 17 13:54:45 2020
+Thread-2: Mon Aug 17 13:54:45 2020
+Thread-1: Mon Aug 17 13:54:46 2020
+Thread-1: Mon Aug 17 13:54:47 2020
+Thread-2: Mon Aug 17 13:54:47 2020
+Thread-1: Mon Aug 17 13:54:48 2020
+退出线程：Thread-1
+Thread-2: Mon Aug 17 13:54:49 2020
+Thread-2: Mon Aug 17 13:54:51 2020
+Thread-2: Mon Aug 17 13:54:53 2020
+退出线程：Thread-2
+退出主线程
+"""
+
+"""
+开始线程：Thread-1
+开始线程：Thread-2
+退出主线程
+Thread-1: Mon Aug 17 13:55:10 2020
+Thread-2: Mon Aug 17 13:55:11 2020
+Thread-1: Mon Aug 17 13:55:11 2020
+Thread-1: Mon Aug 17 13:55:12 2020
+Thread-1: Mon Aug 17 13:55:13 2020
+Thread-2: Mon Aug 17 13:55:13 2020
+Thread-1: Mon Aug 17 13:55:14 2020
+退出线程：Thread-1
+Thread-2: Mon Aug 17 13:55:15 2020
+Thread-2: Mon Aug 17 13:55:17 2020
+Thread-2: Mon Aug 17 13:55:19 2020
+退出线程：Thread-2
+"""
+
+# import queue
+# import threading
+# import time
+#
+# exitFlag = 0
+#
+# class myThread (threading.Thread):
+#     def __init__(self, threadID, name, q):
+#         threading.Thread.__init__(self)
+#         self.threadID = threadID
+#         self.name = name
+#         self.q = q
+#     def run(self):
+#         print ("开启线程：" + self.name)
+#         process_data(self.name, self.q)
+#         print ("退出线程：" + self.name)
+# def process_data(threadName, q):
+#     while not exitFlag:
+#         queueLock.acquire()
+#         if not workQueue.empty():
+#             data = q.get()
+#             queueLock.release()
+#             print ("%s processing %s" % (threadName, data))
+#         else:
+#             queueLock.release()
+#         time.sleep(1)
+#
+# threadList = ["Thread-1", "Thread-2", "Thread-3"]
+# nameList = ["One", "Two", "Three", "Four", "Five"]
+# queueLock = threading.Lock()
+# workQueue = queue.Queue(10)
+# threads = []
+# threadID = 1
+#
+# # 创建新线程
+# for tName in threadList:
+#     thread = myThread(threadID, tName, workQueue)
+#     thread.start()
+#     threads.append(thread)
+#     threadID += 1
+#
+# # 填充队列
+# queueLock.acquire()
+# for word in nameList:
+#     workQueue.put(word)
+# queueLock.release()
+#
+# # 等待队列清空
+# while not workQueue.empty():
+#     pass
+#
+# # 通知线程是时候退出
+# exitFlag = 1
+#
+# # 等待所有线程完成
+# for t in threads:
+#     t.join()
+# print ("退出主线程")
+
+from threading import Thread
+import threading
+
+threadLock = threading.Lock()
+
+threadLock.locked()
+
+threadLock.release()
+
+
+
+
+
